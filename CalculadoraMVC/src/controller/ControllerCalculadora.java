d390cf22ad645fe395226cdc46a89f27e91d7128
Jonathan <84401668+Jonathancastrosilva@users.noreply.github.com>
2025-03-28 09:03:32 -0300

    Com looping

--- CalculadoraMVC/src/controller/ControllerCalculadora.java
+++ CalculadoraMVC/src/controller/ControllerCalculadora.java
@@ -8,11 +8,9 @@ public class ControllerCalculadora {
 
     public double calcular(String operacao, double a, double b) {
 
-        String operacaoUsada = operacao.substring(0, 1).toUpperCase() + operacao.substring(1).toLowerCase();
-
         try {
 
-            Operador operador = (Operador) Class.forName("model."+operacaoUsada).getDeclaredConstructor().newInstance();
+            Operador operador = (Operador) Class.forName("model."+operacao).getDeclaredConstructor().newInstance();
 
             return operador.calcular(a, b);
         }
@@ -20,7 +18,6 @@ public class ControllerCalculadora {
         catch (IllegalAccessException | NoSuchMethodException | ClassNotFoundException | InvocationTargetException | InstantiationException e) {
 
             throw new IllegalArgumentException("Operação inválida!", e);
-
         }
     }
 }
--- CalculadoraMVC/src/view/Menu.java
+++ CalculadoraMVC/src/view/Menu.java
@@ -1,42 +1,55 @@
 package view;
 
 import java.util.Scanner;
+import java.text.Normalizer;
+
 import controller.ControllerCalculadora;
 
 public class Menu {
 
     public static void exibir(){
-        try {
-            Scanner sc = new Scanner(System.in);
-            ControllerCalculadora control = new ControllerCalculadora();
 
-            System.out.println("Soma");
-            System.out.println("Subtração");
-            System.out.println("Multiplicação");
-            System.out.println("Divisão");
-            System.out.println("Potência");
-            System.out.println("Raiz");
-            System.out.println("Digite a operação desejada: ");
+        boolean continua = true;
 
+        Scanner sc = new Scanner(System.in);
+        ControllerCalculadora control = new ControllerCalculadora();
+        
+        while (continua == true){
+
+            System.out.println("Adicao, Subtracao, Multiplicacao, Divisao, Potencia, Raiz ou Sair");
+            System.out.print("Digite a operação desejada: ");
+    
             String operacao = sc.nextLine();
+            operacao = operacao.substring(0, 1).toUpperCase() + operacao.substring(1).toLowerCase();
 
-            System.out.println("Digite o Primeiro Valor");
-            double a = sc.nextDouble();
-            sc.nextLine();
+            if (operacao.equals("Sair")){
+                System.out.println("Saindo");
+                continua = false;
+                break;
+            }
+    
+            try {
 
-            System.out.println("Digite o Segundo Valor");
-            double b = sc.nextDouble();
-            sc.nextLine();
+                System.out.print("Digite o Primeiro Valor: ");
+                double a = sc.nextDouble();
+                sc.nextLine();
+    
+                System.out.print("Digite o Segundo Valor: ");
+                double b = sc.nextDouble();
+                sc.nextLine();
+    
+                System.out.println("Resultado = " + control.calcular(operacao, a, b));
+            }
+    
+            catch (ArithmeticException e) {
 
-            System.out.println(control.calcular(operacao, a, b));
-        }
+                System.out.println("Divisao por 0 não dá");
+            }
+    
+            catch (IllegalArgumentException e) {
 
-        catch (ArithmeticException e) {
-            System.out.println("Divisao por 0 não dá");
-        }
-
-        catch (IllegalArgumentException e) {
-            System.out.println("Operação Invalida");
-        }
+                System.out.println("Operação Invalida");
+            }
+        }   
     }
 }
